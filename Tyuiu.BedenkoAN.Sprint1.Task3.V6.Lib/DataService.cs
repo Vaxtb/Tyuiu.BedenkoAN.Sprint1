d20b93bb7d1f992b6f2eea0a65359f0f4f55d878
Vaxtb <Bedenkoalex123@yandex.ru>
2026-09-26 09:37:51 +0500

    Task3

---
+++ Tyuiu.BedenkoAN.Sprint1.Task3.V6.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.BedenkoAN.Sprint1.Task3.V6.Lib
+{
+    public class DataService : ISprint1Task3V6
+    {
+        public double TravelCost(double distance, double gasFlow, double gasPrice)
+        {
+            return Math.Round(distance * gasFlow * gasPrice * 0.01*2, 3);
+
+        }
+    }
+}
---
+++ Tyuiu.BedenkoAN.Sprint1.Task3.V6.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.BedenkoAN.Sprint1.Task3.V6.Lib;
+namespace Tyuiu.BedenkoAN.Sprint1.Task3.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+
+        {
+            DataService ds = new DataService();
+            double x = 67;
+            double y = 8.5;
+            double z = 6.5;
+            var res = ds.TravelCost(x, y, z);
+            Assert.AreEqual(74.035, res);
+
+        }
+    }
+}
